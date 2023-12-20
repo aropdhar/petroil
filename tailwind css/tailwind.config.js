9dde84d8176858e5dc08b6af4fd8529757292e58
aropdhar <aropsutradhar202@gmail.com>
2023-12-21 00:30:18 +0600

    arop

---
+++ tailwind css/tailwind.config.js
@@ -0,0 +1,35 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          'header': '#282828',
+          'color': '#ffff',
+          'after': '#fac404',
+          'navbar': '#F40404',
+
+        },
+
+        maxWidth: {
+          'container': '1144px',
+        },
+
+        fontSize: {
+          
+          'header-font': '12px',
+          'font-awesome': '15px',
+          
+        },
+        
+        backgroundImage: {
+
+          'bannerbg': " linear-gradient(rgba(0, 0, 0, 0.542),rgba(0, 0, 0, 0.6)) ,url('./images/Group 5.jpg')",
+          
+        },
+        fontFamily: {
+
+          'poppins': "'Poppins', sans-serif",
+          
+        },
+      }
+    }
+  }
