8e515e09a255ffca2954ca29a17659b7e34b010b
aropdhar <aropsutradhar202@gmail.com>
2023-12-25 23:32:32 +0600

    petroil project

--- tailwindcss/tailwind.config.js
+++ tailwindcss/tailwind.config.js
@@ -3,9 +3,11 @@ tailwind.config = {
       extend: {
         colors: {
           'header': '#282828',
-          'color': '#ffff',
+          'color': '#FFFFFF',
           'after': '#fac404',
           'navbar': '#F40404',
+          'btn': 'linear-gradient(rgba(255, 255, 255, 0.5))',
+          'footerbg': '#1F1F1F',
 
         },
 
@@ -23,7 +25,21 @@ tailwind.config = {
         backgroundImage: {
 
           'bannerbg': " linear-gradient(rgba(0, 0, 0, 0.542),rgba(0, 0, 0, 0.6)) ,url('./images/Group 5.jpg')",
-          
+
+          // service section
+
+          'servicebg': " linear-gradient(rgba(0, 0, 0, 0.542),rgba(0, 0, 0, 0.6)) ,url('./images/service_1.png')",
+          'servicebg2': " linear-gradient(rgba(0, 0, 0, 0.542),rgba(0, 0, 0, 0.6)) ,url('./images/service_2.png')",
+          'servicebg3': " linear-gradient(rgba(0, 0, 0, 0.542),rgba(0, 0, 0, 0.6)) ,url('./images/service_3.png')",
+
+        //  blog section
+
+        'blogbg': " linear-gradient(rgba(0, 0, 0, 0.542),rgba(0, 0, 0, 0.6)) ,url('./images/unsplash_1.png')",
+
+        'blogbg2': " linear-gradient(rgba(0, 0, 0, 0.542),rgba(0, 0, 0, 0.6)) ,url('./images/unsplash_2.png')",
+
+        'blogbg3': " linear-gradient(rgba(0, 0, 0, 0.542),rgba(0, 0, 0, 0.6)) ,url('./images/unsplash_3.png')",
+
         },
         fontFamily: {
 
